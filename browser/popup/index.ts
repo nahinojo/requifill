f1ec4a093453ff313538159795d34d83b78dd5bc
nahinojo <noah.hinojos@gmail.com>
2023-03-16 11:15:46 -0700

    feat: configured Webpack to utilize Typescript

---
+++ browser/popup/index.ts
@@ -0,0 +1,11 @@
+// create the header element
+const header = document.createElement('header');
+
+// add some text to the header
+const headerText = document.createTextNode('This is the header');
+
+// append the text to the header element
+header.appendChild(headerText);
+
+// append the header element to the document body
+document.body.appendChild(header);
