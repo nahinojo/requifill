b43aa3f1e5c993f8ac42bed877e16d25bc715758
nahinojo <noah.hinojos@gmail.com>
2023-03-23 20:37:10 -0700

    fix: disabled logAllEvents()

--- src/popup/index.tsx
+++ src/popup/index.tsx
@@ -3,18 +3,18 @@ import {createRoot} from 'react-dom/client';
 import App from './App'
 import './styles/popup.css'
 
-// PRODCUTION USE ONLY
-function logAllEvents() {
-  for (const eventType in window) {
-    if (eventType.startsWith("on")) {
-      window.addEventListener(eventType.substring(2), function(event) {
-        console.log(eventType.substring(2) + " event:", event);
-      });
-    }
-  }
-}
+// // PRODCUTION USE ONLY
+// function logAllEvents() {
+//   for (const eventType in window) {
+//     if (eventType.startsWith("on")) {
+//       window.addEventListener(eventType.substring(2), function(event) {
+//         console.log(eventType.substring(2) + " event:", event);
+//       });
+//     }
+//   }
+// }
 
-logAllEvents();
+// logAllEvents();
 
 const root = document.getElementById('root') as HTMLDivElement | null;
 root && createRoot(root).render(<App />);
