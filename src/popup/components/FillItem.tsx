31ef9ca5cef16d8d9bc52624108d2569d0cbf09c
nahinojo <noah.hinojos@gmail.com>
2023-03-27 13:42:48 -0700

    feat: stored fill data in browser's sync storage

--- src/popup/components/FillItem.tsx
+++ src/popup/components/FillItem.tsx
@@ -5,12 +5,15 @@ import
     InputHTMLAttributes, 
     LabelHTMLAttributes,
     FC,
+    ReactEventHandler,
+    ChangeEvent,
+    SyntheticEvent,
   }
 from 'react';
 
 type HTMLProps = Pick<
   HTMLAttributes<HTMLElement>, 
-  "className" | "id"
+  "id"
 >;
 type LabelProps = Pick<
   LabelHTMLAttributes<HTMLLabelElement>, 
@@ -24,8 +27,20 @@ interface FillItemProps extends HTMLProps, LabelProps, InputProps {
   title: string
 };
 
+interface OnBlurEvent extends ChangeEvent<HTMLInputElement> {};
+
 const FillItem: FC<FillItemProps> = (props) => {
 
+  const saveValue: ReactEventHandler<HTMLInputElement> = async (
+    evt: OnBlurEvent
+  ) => {
+    console.log(evt)
+    console.log("Object name: ", evt.target.name)
+    const { name } = evt.target
+    const syncStorage = await browser.storage.sync.set({
+      [name]: props.value
+    })
+  }
   return (
     <div 
       className="fill-item-wrapper"
@@ -34,18 +49,18 @@ const FillItem: FC<FillItemProps> = (props) => {
       <label 
         className="fill-item-label"
         id={props.id+"-label"} 
-        htmlFor="fill-item-input"
+        htmlFor={props.id+"-input"} 
         >{props.title}</label>
       <input
         className="fill-item-input"
         name={props.name}
         id={props.id+"-input"}
+        value={props.value}
         type={props.type}
         pattern={props.pattern}
-        value={props.value}
-        onChange={props.onChange}
         inputMode={props.inputMode}
-        onBlur={(e)=>(console.log(e))}
+        onChange={props.onChange}
+        onBlur={saveValue}
       />
     </div>
   );
