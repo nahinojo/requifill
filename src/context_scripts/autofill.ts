5fbcef31e25f8571528c74ebef8f7f0297a5a9e9
nahinojo <noah.hinojos@gmail.com>
2023-03-28 19:44:14 -0700

    feat: autofill script is now bundled from Typescript file

--- src/context_scripts/autofill.ts
+++ src/context_scripts/autofill.ts
@@ -1,16 +1,20 @@
-const syncStorage = browser.storage.sync
+const syncStorage: browser.storage.StorageAreaSync = browser.storage.sync
 
-const nameToIdDict = {
+interface NameToIdDict {
+    [key: string]: string;
+}
+
+const nameToIdDict: NameToIdDict = {
     adHocUserId: "newAdHocRoutePerson.id",
     commodityCode: "newPurchasingItemLine.purchasingCommodityCode",
     requestorPersonPhoneNumber: "document.requestorPersonPhoneNumber"  
 }
 
-const fillRequisitionForm = async() => {
+const fillRequisitionForm = async(): Promise<void> => {
     const storedFillValues = await syncStorage.get(null)
     for (let name in storedFillValues) {
         const fillValue = storedFillValues[name]
-        const targetInput = document.getElementById(nameToIdDict[name])
+        const targetInput = document.getElementById(nameToIdDict[name]) as HTMLInputElement
         if (targetInput.value !== fillValue) {
             targetInput.value = storedFillValues[name]
         }
