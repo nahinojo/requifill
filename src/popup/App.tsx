2df344399ffc6a7d54d8cc2d5b9b46ae3d6896ac
nahinojo <noah.hinojos@gmail.com>
2023-04-13 14:47:39 -0700

    docs: commented speficity for browser storage updater

--- src/popup/App.tsx
+++ src/popup/App.tsx
@@ -18,6 +18,7 @@ const App: FC = () => {
   })
 
   useEffect(() => {
+  // Ensures browser storage is up tp date.
     browser.storage.sync.get()
       .then((fillValuesSync) => {
         for (const name in fillValuesSync) {
