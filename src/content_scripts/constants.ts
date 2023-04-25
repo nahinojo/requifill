75e25a43ef613ec0e1ba1564e45be57ba51eae82
nahinojo <noah.hinojos@gmail.com>
2023-04-25 08:59:02 -0700

    fix: type-set isProperURL as boolean

--- src/content_scripts/constants.ts
+++ src/content_scripts/constants.ts
@@ -5,6 +5,6 @@ import { minimatch } from 'minimatch'
 
 const windowURL: string = window.location.href
 const expectedURL = 'https://systems.oit.uci.edu/kfs/purapRequisition.do*'
-export const isProperURL = minimatch(windowURL, expectedURL)
+export const isProperURL: boolean = minimatch(windowURL, expectedURL)
 
 export const syncStorage: browser.storage.StorageAreaSync = browser.storage.sync
