adc619a80ba702944c23c3a1906f1574763ac4c1
nahinojo <noah.hinojos@gmail.com>
2023-05-07 13:48:00 -0700

    feat: added custom tailwind colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,22 @@
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ['./src/**/*.{js,ts,tsx}'],
   plugins: [],
   theme: {
+    colors: {
+      /* eslint-disable sort-keys */
+      white: '#FFFFFF',
+      bleach: '#D6D8DC',
+      platinum: '#ACB0B8',
+      silver: '#838995',
+      iron: '#6A6F7C',
+      wither: '#525660',
+      thunder: '#3B3E45',
+      storm: '#2F3237',
+      night: '#232529',
+      abyss: '#111113'
+    },
     extend: {}
   }
 }
