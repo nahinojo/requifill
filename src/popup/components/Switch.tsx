8fdf600b8b7a5cebb4ee152b60edc586ea11c12b
nahinojo <noah.hinojos@gmail.com>
2023-05-15 16:21:02 -0700

    feat: resolved Switch render issue

--- src/popup/components/Switch.tsx
+++ src/popup/components/Switch.tsx
@@ -8,6 +8,7 @@ const Switch: React.FC<SwitchProps> = ({ className }) => {
   console.log('Render ID:', Math.random())
   // Consider passing in the stored isAutofill as a prop.
   const [isAutofill, setIsAutofill] = useState<boolean>(false)
+  const [isLoading, setIsLoading] = useState<boolean>(true)
   const handleAutofillChange: ReactEventHandler<HTMLInputElement> = () => {
     syncStorage.set({
       settings: {
@@ -27,11 +28,13 @@ const Switch: React.FC<SwitchProps> = ({ className }) => {
       console.log('useEffect(): isAutofill:', isAutofill)
       console.log('useEffect(): isAutofillStorage:', isAutofillStorage)
       setIsAutofill(isAutofillStorage)
+      setIsLoading(false)
     }).catch(error => {
       console.log(error)
     })
   }, [])
   console.log('isAutofill:', isAutofill)
+  if (isLoading) return <div className='w-10'></div>
   return (
     <>
       <input
