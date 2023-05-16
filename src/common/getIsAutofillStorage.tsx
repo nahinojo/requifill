c3c61ecd32799ceb5a255046e76e8e3fa769ef59
nahinojo <noah.hinojos@gmail.com>
2023-05-16 15:03:22 -0700

    fix: corrected storage value call

--- src/common/getIsAutofillStorage.tsx
+++ src/common/getIsAutofillStorage.tsx
@@ -6,7 +6,7 @@ Retrieves setting of whether autofill is toggled on/off.
 const getIsAutofillStorage = async (): Promise<boolean> => {
   return await syncStorage.get('settings')
     .then(storage => {
-      return Boolean(storage.settings?.isAutofillToggled)
+      return Boolean(storage?.settings?.isAutofill)
     }).catch(error => {
       console.log(error)
       return false
