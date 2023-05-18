e778721fdad51188566f5da1e13dacfb06b32c97
nahinojo <noah.hinojos@gmail.com>
2023-05-18 16:24:17 -0700

    fix: ensured autofill.ts only operates on req form

--- src/content_scripts/autofill.ts
+++ src/content_scripts/autofill.ts
@@ -6,63 +6,92 @@ import syncStorage from '../common/syncStorage'
 import isProperURL from '../common/isProperURL'
 import getIsAutofillStorage from '../common/getIsAutofillStorage'
 
-/*
-Since actualy DOM IDs are absurdly long, a shorthand translation dict is used
-*/
-type NameToIdDictKeys = 'adHocUserId' | 'commodityCode' | 'roomNumber'
-const nameToIdDict = {
-  adHocUserId: 'newAdHocRoutePerson.id',
-  commodityCode: 'newPurchasingItemLine.purchasingCommodityCode',
-  roomNumber: 'document.deliveryBuildingRoomNumber'
-}
-
-/*
-Uses field data from browser storage to autofill <input> elements
-*/
-const autofill = (): void => {
-  syncStorage.get()
-    .then(storage => {
-      const { fieldData } = storage
-      for (const fieldName in fieldData) {
-        const fieldValue = fieldData[fieldName]
-        const targetInput = document.getElementById(
-          nameToIdDict[fieldName as NameToIdDictKeys]
-        ) as HTMLInputElement
-        // Prevents duplicate injections of adHocUserId
-        const neglectAdHocUserId = (
-          fieldName === 'adHocUserId' &&
-              document.getElementById('adHocRoutePerson[0].id') !== null
-        )
-        if (targetInput.value !== fieldValue && !neglectAdHocUserId) {
-          targetInput.value = fieldValue
-        }
-      }
-    })
-    .catch(error => {
-      console.log(error)
-    })
-}
-
-/*
-Executes on initial page load
-*/
-getIsAutofillStorage().then(isAutofill => {
-  if (isAutofill && isProperURL) {
-    autofill()
+if (isProperURL) {
+  /*
+  Since actualy DOM IDs are absurdly long, a shorthand translation dict is used
+  */
+  type NameToIdDictKeys = 'adHocUserId' | 'commodityCode' | 'roomNumber'
+  const nameToIdDict = {
+    adHocUserId: 'newAdHocRoutePerson.id',
+    commodityCode: 'newPurchasingItemLine.purchasingCommodityCode',
+    roomNumber: 'document.deliveryBuildingRoomNumber'
   }
-}).catch(error => {
-  console.log(error)
-})
 
-/*
-Executes when 'Enable Autofill' switch is toggled
-*/
-syncStorage.onChanged.addListener(() => {
+  /*
+  Uses field data from browser storage to autofill requisition form <input> elements
+  */
+  const autofill = (): void => {
+    syncStorage.get()
+      .then(storage => {
+        const { fieldData } = storage
+        for (const fieldName in fieldData) {
+          const fieldValue = fieldData[fieldName]
+          const targetInput = document.getElementById(
+            nameToIdDict[fieldName as NameToIdDictKeys]
+          ) as HTMLInputElement
+          // Prevents duplicate injections of adHocUserId
+          const neglectAdHocUserId = (
+            fieldName === 'adHocUserId' &&
+                document.getElementById('adHocRoutePerson[0].id') !== null
+          )
+          if (targetInput.value !== fieldValue && !neglectAdHocUserId) {
+            targetInput.value = fieldValue
+          }
+        }
+      })
+      .catch(error => {
+        console.log(error)
+      })
+  }
+  /*
+  Empties all field data from all requisition form <input> elements
+  */
+  const autoclear = (): void => {
+    syncStorage.get()
+      .then(storage => {
+        const { fieldData } = storage
+        for (const fieldName in fieldData) {
+          const targetInput = document.getElementById(
+            nameToIdDict[fieldName as NameToIdDictKeys]
+          ) as HTMLInputElement
+          // Prevents duplicate injections of adHocUserId
+          const neglectAdHocUserId = (
+            fieldName === 'adHocUserId' &&
+                document.getElementById('adHocRoutePerson[0].id') !== null
+          )
+          if (!neglectAdHocUserId) {
+            targetInput.value = ''
+          }
+        }
+      })
+      .catch(error => {
+        console.log(error)
+      })
+  }
+
+  /*
+  Executes on initial page load
+  */
   getIsAutofillStorage().then(isAutofill => {
-    if (isAutofill && isProperURL) {
+    if (isAutofill) {
       autofill()
     }
   }).catch(error => {
     console.log(error)
   })
-})
+
+  /*
+  Autofills when corresponding switch is toggled
+  */
+  syncStorage.onChanged.addListener(() => {
+    getIsAutofillStorage().then(isAutofill => {
+      if (isAutofill) {
+        autofill()
+      } else {
+        autoclear()
+      }
+    }).catch(error => {
+      console.log(error)
+    })
+  })
+}
