e7ee80e44da22839ba5ce7f76259571504975e36
Noah Hinojos <noah.hinojos@gmail.com>
2023-06-22 09:29:45 -0700

    feat: import + as SVG WIP

--- src/popup/components/NewFieldItem.tsx
+++ src/popup/components/NewFieldItem.tsx
@@ -1,15 +1,31 @@
 import React, { useState } from 'react'
 import type { FC } from 'react'
+import PlusSVG from './plus.svg'
 
 const NewFieldItem: FC = (props) => {
   const [isSelections, setIsSelections] = useState(false)
   return (
       <div onClick={() => { setIsSelections(true) }}>
         {!isSelections &&
-          <button>New Field Item</button>
+        <div
+          className='flex justify-center'
+        >
+          <PlusSVG />
+          <button
+            className='text-sm text-silver mt-1'
+          >New Field Item</button>
+        </div>
         }
         {isSelections &&
-          <div></div>
+          <div
+            className='flex flex-col'
+          >
+            <h1>New Autofill Field</h1>
+            <button onClick={ () => { setIsSelections(false) }}>Field Item 1</button>
+            <button onClick={ () => { setIsSelections(false) }}>Field Item 2</button>
+            <button onClick={ () => { setIsSelections(false) }}>Field Item 3</button>
+            <button onClick={ () => { setIsSelections(false) }}>Field Item 4</button>
+          </div>
         }
       </div>
   )
