ac0b2556849d537aec265f485d56ea5e0871294c
Noah Hinojos <noah.hinojos@gmail.com>
2023-07-05 14:26:36 -0700

    feat: styled new field selection menu

--- src/popup/components/NewFieldItem.tsx
+++ src/popup/components/NewFieldItem.tsx
@@ -4,29 +4,50 @@ import PlusSVG from './PlusSVG'
 
 const NewFieldItem: FC = (props) => {
   const [isSelections, setIsSelections] = useState(false)
+  const buttonFieldItemStyling = 'text-left bg-night border border-solid border-wither pl-3 py-2'
   return (
-      <div onClick={() => { setIsSelections(true) }}>
+      <div
+        className='mx-1 mt-1.5'
+        onClick={() => { setIsSelections(true) }}
+      >
         {!isSelections &&
         <div
-          className='flex justify-center mx-28 cursor-pointer'
+          id='new-field-initiator'
+          className='flex justify-center w-fit mx-auto cursor-pointer'
         >
-          <PlusSVG 
-            className='mt-1 mr-1'
+          <PlusSVG
+            className='mr-1 mt-0.5'
           />
           <button
-            className='text-sm text-silver mt-1'
-          >New Field Item</button>
+          id='new-field'
+            className='text-sm text-silver'
+          >New Field</button>
         </div>
         }
         {isSelections &&
           <div
+            id='field-selections'
             className='flex flex-col'
           >
-            <h1>New Autofill Field</h1>
-            <button onClick={ () => { setIsSelections(false) }}>Field Item 1</button>
-            <button onClick={ () => { setIsSelections(false) }}>Field Item 2</button>
-            <button onClick={ () => { setIsSelections(false) }}>Field Item 3</button>
-            <button onClick={ () => { setIsSelections(false) }}>Field Item 4</button>
+            <h1
+              className='mx-auto font-bold text-sm'
+            >New Autofill Field</h1>
+            <button 
+              className={`${buttonFieldItemStyling} border-b-0 rounded-t-md`}
+              onClick={ () => { setIsSelections(false) }}
+            >Field Item 1</button>
+            <button 
+              className={`${buttonFieldItemStyling} border-b-0`}
+              onClick={ () => { setIsSelections(false) }}
+            >Field Item 2</button>
+            <button
+              className={`${buttonFieldItemStyling} border-b-0`}
+              onClick={ () => { setIsSelections(false) }}
+            >Field Item 3</button>
+            <button
+              className={`${buttonFieldItemStyling} rounded-b-md`}
+              onClick={ () => { setIsSelections(false) }}
+            >Field Item 4</button>
           </div>
         }
       </div>
