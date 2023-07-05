a249b28150ad4a4db3eb40db2b4a5eb28b543d69
Noah Hinojos <noah.hinojos@gmail.com>
2023-07-05 14:26:53 -0700

    fix: shrunk plus SVG

--- src/popup/components/PlusSVG.tsx
+++ src/popup/components/PlusSVG.tsx
@@ -6,11 +6,12 @@ interface PlusSVGProps extends HTMLAttributes<SVGElement> {}
 const PlusSVG: FC<PlusSVGProps> = ({ className }) => {
   return (
     <svg
+      id='plus-svg'
       className={`${className}`}
       viewBox="0 0 448 512"
       xmlns="http://www.w3.org/2000/svg"
-      width="16"
-      height="16"
+      width="14"
+      height="14"
     >
       <path
         d="m416 208h-144v-144c0-17.67-14.33-32-32-32h-32c-17.67 0-32 14.33-32 32v144h-144c-17.67 0-32 14.33-32 32v32c0 17.67 14.33 32 32 32h144v144c0 17.67 14.33 32 32 32h32c17.67 0 32-14.33 32-32v-144h144c17.67 0 32-14.33 32-32v-32c0-17.67-14.33-32-32-32z"
